be9d27b955fc6be8cdedcabfbbd3682d367d693b
Niklas Schachl <niklas.schachl@htl-wels.at>
2021-01-21 11:00:55 +0100

    Graphical User Interface finished

--- src/at/nsc/controller/MainController.java
+++ src/at/nsc/controller/MainController.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -22,9 +23,14 @@ public class MainController implements Initializable
 
     @FXML
     private Label label_color;
-
     @FXML
     private Label label_hex;
+    @FXML
+    private TextField textField_red;
+    @FXML
+    private TextField textField_green;
+    @FXML
+    private TextField textField_blue;
 
     public static void show(Stage stage)
     {
@@ -56,5 +62,51 @@ public class MainController implements Initializable
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
 
+    }
+
+    @FXML
+    private void action_red_plus()
+    {
+        increaseColour("red");
+    }
+
+    @FXML
+    private void action_red_minus()
+    {
+        decreaseColour("red");
+    }
+
+    @FXML
+    private void action_green_plus()
+    {
+        increaseColour("green");
+    }
+
+    @FXML
+    private void action_green_minus()
+    {
+        decreaseColour("green");
+    }
+
+    @FXML
+    private void action_blue_plus()
+    {
+        increaseColour("blue");
+    }
+
+    @FXML
+    private void action_blue_minus()
+    {
+        decreaseColour("blue");
+    }
+
+    private void increaseColour(String colour)
+    {
+        System.out.println("Test +");
+    }
+
+    private void decreaseColour(String colour)
+    {
+        System.out.println("Test -");
     }
 }
